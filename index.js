94a4e9d475be04c4fca32e74df1b11e7d38fc532
Jalen Peeler <jalenpeeler@yahoo.com>
2021-04-04 06:07:51 -0400

    Done filter

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter( match =>
+      match.toLowerCase() === string.toLowerCase()
+    )
+  }
+  
+  function fuzzyMatch(drivers2, string2) {
+    return drivers2.filter( match =>
+      match.toLowerCase().indexOf(string2.toLowerCase()) === 0
+    )
+  }
+  
+  function matchName(drivers3, string3) {
+    return drivers3.filter( record => record.name === string3)
+  }
